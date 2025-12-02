6cf573a2bbd70413e3fe8851a71f208bed52c0a9
SaidaDev <saida_musaxonova@student.itpu.uz>
2025-12-02 20:58:56 +0500

    feat: do some codewars

---
+++ app.js
@@ -0,0 +1,43 @@
+//1-masala
+
+// function numberToString(num) {
+//  return num.toString()
+// }
+
+
+//2-masala
+
+// function solution(str){
+//   let a = str.split("")
+//   return a.reverse().join("")
+// }
+
+// console.log(solution("hello"));
+
+//3-masala
+
+// function repeatStr (n, s) {
+//   return s.repeat(n)
+// }
+
+// console.log(repeatStr(3,"hello"));
+
+
+//4-masala
+
+
+// function removeChar(str){
+// return str.slice(1,str.length-1)
+// };
+
+// console.log(removeChar("hello"));
+
+
+//5-masala
+
+// const stringToNumber = function(str){
+//   return Number(str)
+//   return null;
+// }
+
+// console.log(stringToNumber("123"));
