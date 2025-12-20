d8dce04159ddd138b6d282f9e1ef164e36bb0481
SaidaDev <saida_musaxonova@student.itpu.uz>
2025-12-20 09:22:44 +0500

    feat: do some coding

--- app.js
+++ app.js
@@ -208,3 +208,21 @@
 // }
 
 // console.log(reverseWords("hello world"));
+
+// 25-masala
+
+// function strCount(str, letter){ 
+//   let count = 0;
+  
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i]=== letter) {
+//       count ++
+//     }else{
+//       count
+//     }
+//   }
+//   return count
+// }
+// console.log(strCount("hello","l"));
+
+
